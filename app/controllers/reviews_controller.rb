db3514fb42a67a7ff423759abf45b5402439e339
Gaurav Koley <arkokoley@live.in>
2017-12-31 18:59:22 +0530

    bug fixes in reviews and registrations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -64,7 +64,7 @@ class ReviewsController < ApplicationController
 
   private
     def authenticate_editor
-      redirect_to root_url unless @abstract_doc.user == current_user
+      redirect_to root_url unless @review.user == current_user
     end
 
     # Use callbacks to share common setup or constraints between actions.
